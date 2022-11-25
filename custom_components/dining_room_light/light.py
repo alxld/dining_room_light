c379b051df1e9a4858689cc4f76a24e8b4eb4ca9
alxld <aaronandlouise@yahoo.com>
2022-11-25 23:59:30 +0000

    Testing

--- custom_components/dining_room_light/light.py
+++ custom_components/dining_room_light/light.py
@@ -38,6 +38,7 @@ class DiningRoomLight(NewLight):
         self.entities["light.dining_room_group"] = None
         self.switch = "Dining Room Switch"
         self.motion_sensors.append("Dining Room Motion Sensor")
+
         self.has_brightness_threshold = True
         self.brightness_threshold = 128
         self.motion_sensor_brightness = 128
